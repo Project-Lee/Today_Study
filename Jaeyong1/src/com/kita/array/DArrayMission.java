df6daca4a502b6574c6994c3c85b9abc61589685
ddw0099 <ddw0099@naver.com>
2020-06-17 17:40:29 +0900

    Test1

---
+++ Jaeyong1/src/com/kita/array/DArrayMission.java
@@ -0,0 +1,40 @@
+package com.kita.array;
+
+public class DArrayMission {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * 영어합계 :  / 평균 : 
+		 * 수학합계 :  / 평균 : 
+		 * 국어합계 :  / 평균 : 
+		 * 전체합계 :  / 평균 : 
+		 */
+		
+		String[] sub = {"영어","수학","국어"};  // 과목별 선택
+		
+		int[][] arr = {
+				{90, 100, 88},  // 영어
+				{34, 99, 45},	// 수학
+				{98, 65, 87}	// 국어
+		};
+		
+		
+		int maxTotal = 0; // 3과목 전체 합을 담을 변수
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			int total = 0; // 한과목당 합계누적
+			for(int j=0; j<arr.length; j++) {
+				
+				total += arr[i][j];
+				maxTotal += arr[i][j];
+			}
+			
+			System.out.printf("%s 점수 합계 : %d // 평균 : %.1f\n\n",sub[i],total,total / 3.0);
+		}
+			System.out.printf("전체 합계 : [ %d ]",maxTotal);
+	
+	}
+
+}
